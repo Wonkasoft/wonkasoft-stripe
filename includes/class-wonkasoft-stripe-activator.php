81959aa33223e3abc82284acfca79a6dde22cd43
llister15 <llister@wonkasoft.com>
2019-11-21 13:59:39 -0800

    init

---
+++ includes/class-wonkasoft-stripe-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wonkasoft.com
+ * @since      1.0.0
+ *
+ * @package    Wonkasoft_Stripe
+ * @subpackage Wonkasoft_Stripe/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wonkasoft_Stripe
+ * @subpackage Wonkasoft_Stripe/includes
+ * @author     Wonkasoft <support@wonkasoft.com>
+ */
+class Wonkasoft_Stripe_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
